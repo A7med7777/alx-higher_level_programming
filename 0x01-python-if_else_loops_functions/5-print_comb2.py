1720275214cc4d0bfdc9659d2e8a6cda6c339fd0
A7med7777 <ahmedhessian56@gmail.com>
2023-11-29 03:47:17 +0200

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(99):
-    if i < 10:
-        print("0", end="")
-    print(i, end=", ")
+    first = i / 10
+    last = i % 10
+    print("{:d}{:d}".format(int(first), int(last)), end=", ")
 print(99)
